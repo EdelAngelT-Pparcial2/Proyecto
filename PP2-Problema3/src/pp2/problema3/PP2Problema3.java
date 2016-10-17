34bd6739176c6a82bff5e45899764b484b02e10a
Alumno <Alumno@328-28>
2016-10-17 12:22:11 -0500

    problema3

---
+++ PP2-Problema3/src/pp2/problema3/PP2Problema3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema3;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class PP2Problema3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double factorial=1;
+        int numero;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce numero");
+        numero=teclado.nextInt();
+        for (int a=numero)
+        
+    }
+  
+        
+    }
+    
