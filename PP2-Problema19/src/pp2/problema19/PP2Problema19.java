36263708e97cca4541033ee756ee6973ae78aaf1
LuisGuillermo <LuisGuillermo@memo.lan>
2016-10-19 16:02:12 -0500

    problema 19

---
+++ PP2-Problema19/src/pp2/problema19/PP2Problema19.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema19;
+import java.util.*;
+/**
+ *
+ * @author LuisGuillermo
+ */
+public class PP2Problema19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double angulo;
+        angulo= solicitarAngulo();
+        resultado(angulo);
+    }
+    public static double solicitarAngulo(){
+        double angulo;
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Introduce angulo");
+        angulo = teclado.nextDouble();
+        return angulo;
+    }
+   public static void resultado(double angulo){
+       double a= Math.toRadians(angulo);
+       System.out.println(Math.cos(angulo) + " coseno");
+       System.out.println(Math.sin(angulo) + " seno");
+       System.out.println(Math.tan(angulo) + " tangente");
+   }
+}
