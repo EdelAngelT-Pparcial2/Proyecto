e99ac37d3064de4b8e71c322196667cf66e29098
EdelAngel97 <esteban-9709@hotmail.com>
2016-10-18 23:57:32 -0500

    Update PP2Problema3.java

--- PP2-Problema3/src/pp2/problema3/PP2Problema3.java
+++ PP2-Problema3/src/pp2/problema3/PP2Problema3.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,15 +17,24 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double factorial=1;
+         int numero;
+        numero= solicitarNumero();
+        factorial(numero);
+         }
+    public static int solicitarNumero(){
         int numero;
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce numero");
-        numero=teclado.nextInt();
-        for (int i=numero;i>0;i--){
-            factorial= factorial * i;
+        numero= teclado.nextInt();
+        return numero;
+    }
+    public static void factorial(int numero){
+        int resultado=1;
+        for(int i = numero; i>0 ;i--){
+            resultado= resultado * i;
         }
-        System.out.println("El factorial de "+ numero + " es " + factorial);
+        
+     System.out.println("El factorial de "+ numero + " es " + resultado);
     }
     
 }
