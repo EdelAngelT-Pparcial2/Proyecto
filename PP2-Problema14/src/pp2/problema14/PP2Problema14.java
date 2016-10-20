bea168f97c5fa3da2abd3082ec82ef9374298241
LuisGuillermo <LuisGuillermo@memo>
2016-10-20 12:04:16 -0500

    problema 14

---
+++ PP2-Problema14/src/pp2/problema14/PP2Problema14.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema14;
+/**
+ *
+ * @author LuisGuillermo
+ */
+public class PP2Problema14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][] tabla = new int [7][7];
+        mostrarTabla(tabla);
+    }
+    public static void mostrarTabla(int [][]a){
+        int i,p,q = 0;
+        for(i=0;i<7;i++){
+            for(p=0;p<7;p++){
+                if(i==p){
+                    q=1;
+                }else{
+                    q=0;
+                            
+                            }
+                System.out.print(q + " ");
+                }
+               System.out.println(); 
+            }
+        
+        }
+        
+    }
+      
