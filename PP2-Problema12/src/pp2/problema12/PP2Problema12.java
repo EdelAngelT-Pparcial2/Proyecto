66f53f7718e4d52472c23966480eb5fcbc4756a8
LuisGuillermo <LuisGuillermo@memo>
2016-10-20 14:15:02 -0500

    problema 12

---
+++ PP2-Problema12/src/pp2/problema12/PP2Problema12.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema12;
+import java.util.*;
+/**
+ *
+ * @author LuisGuillermo
+ */
+public class PP2Problema12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int [] media = new int[10];
+       media= solicitarNumero(media);
+       mostrar(media);
+    }
+    public static int[] solicitarNumero(int []a){
+        int i=0;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce numero");
+        while(i<a.length){
+        a[1]= teclado.nextInt();
+        i++;
+        }
+        return a;
+        }
+    public static void mostrar(int []a){
+       int p=0,q=0;
+       for(int i=1;i<a.length;i+=2){
+           p=a[i]+p;
+          q++;
+       }
+       p=p/q;
+       System.out.println("La media de los numeros es:" + p);
+    }
+}
+ 
