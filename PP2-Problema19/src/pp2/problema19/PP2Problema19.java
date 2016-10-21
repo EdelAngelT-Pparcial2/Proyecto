adae3f897fa2ed3c155d7635a808d8afd4be1ff3
EdelAngel97 <esteban-9709@hotmail.com>
2016-10-20 21:47:08 -0500

    Update PP2Problema19.java

--- PP2-Problema19/src/pp2/problema19/PP2Problema19.java
+++ PP2-Problema19/src/pp2/problema19/PP2Problema19.java
@@ -20,14 +20,14 @@ public class PP2Problema19 {
         angulo= solicitarAngulo();
         resultado(angulo);
     }
-    public static double solicitarAngulo(){
+    public static double solicitarAngulo(){//Se introduce el angulo que usaremos
         double angulo;
         Scanner teclado = new Scanner (System.in);
         System.out.println("Introduce angulo");
         angulo = teclado.nextDouble();
         return angulo;
     }
-   public static void resultado(double angulo){
+   public static void resultado(double angulo){//Se calcula cual es el coseno,seno,tangente del angulo
        double a= Math.toRadians(angulo);
        System.out.println(Math.cos(angulo) + " coseno");
        System.out.println(Math.sin(angulo) + " seno");
