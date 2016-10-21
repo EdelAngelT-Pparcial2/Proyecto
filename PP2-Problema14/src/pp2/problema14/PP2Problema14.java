caf04ca1600caf45423215236ac36ff31b2debeb
EdelAngel97 <esteban-9709@hotmail.com>
2016-10-20 21:41:58 -0500

    Update PP2Problema14.java

--- PP2-Problema14/src/pp2/problema14/PP2Problema14.java
+++ PP2-Problema14/src/pp2/problema14/PP2Problema14.java
@@ -13,12 +13,12 @@ public class PP2Problema14 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Se hace una tabla con un arreglo de 7x7
         // TODO code application logic here
         int [][] tabla = new int [7][7];
         mostrarTabla(tabla);
     }
-    public static void mostrarTabla(int [][]a){
+    public static void mostrarTabla(int [][]a){//Se muestra un numero 1 cuando la coordenada es la misma y un 0 cuando no
         int i,p,q = 0;
         for(i=0;i<7;i++){
             for(p=0;p<7;p++){
