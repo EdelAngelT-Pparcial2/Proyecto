8a759d518776449c3a026f41a8eb27acb3c8c778
EdelAngel97 <esteban-9709@hotmail.com>
2016-10-20 21:25:56 -0500

    Update PP2Problema8.java

--- PP2-Problema8/src/pp2/problema8/PP2Problema8.java
+++ PP2-Problema8/src/pp2/problema8/PP2Problema8.java
@@ -23,21 +23,21 @@ public class PP2Problema8 {
         hipotenusa(c1,c2);
         
     }
-    public static int solicitarCateto1(){
+    public static int solicitarCateto1(){//Se ingresa el primer cateto
         int c1;
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introducir cateto 1");
         c1= teclado.nextInt();
         return c1;
     }
-    public static int solicitarCateto2(){
+    public static int solicitarCateto2(){//Se ingresa el primer cateto
         int c2;
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introducir cateto 2");
         c2= teclado.nextInt();
         return c2;
     }
-    public static void hipotenusa(int c1, int c2){
+    public static void hipotenusa(int c1, int c2){//Se calcula la hipotenusa
         double h;
         h = Math.pow(c1,2)+ Math.pow(c2,2);
         System.out.println("la hipotenusa es de " + h);
